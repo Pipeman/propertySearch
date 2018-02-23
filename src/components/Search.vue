8c4494b8cc3f5a86348ef760640a74886ea32d5b
gobbot <tiziano.delgobbo@capita.co.uk>
2018-02-23 18:21:24 +0000

    first commit

---
+++ src/components/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="search">
+    <h1>{{title}}</h1>
+    <form>
+      <input type="text" v-model="q">
+      <button
+        type="submit"
+        @click="submit"
+      >Search</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      title: 'Search for houses and flats for sale across the UK',
+      q: '',
+    };
+  },
+  methods: {
+    submit() {
+      fetch("https://property-search-jlrslqbnta.now.sh/search", { q: this.q })
+        .then(response => {
+          if (!response.ok) {
+            console.error('wrongsearch');
+            
+          }
+          return response;
+        })
+        .then(response => {
+          console.error(response.text());
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
